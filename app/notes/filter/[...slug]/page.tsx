d38a19548bc87247c9e96ffdff50c11772d4c02c
Vira Khan <vira.khan1@gmail.com>
2025-10-10 23:17:25 +0200

    update

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -7,39 +7,13 @@ import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
 import { type Category, type CategoryNoAll } from "@/types/note";
 import { notFound } from "next/navigation";
-import type { Metadata } from "next";
+
 
 type Props = {
   params: Promise<{ slug?: string[] }>;
 };
 
-export const dynamicParams = false;
-export const revalidate = 900;
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug = [] } = await params;
-  const note = slug[0] || "All";
-
-  return {
-    title: `Notes: ${note}`,
-    description: `Notes filtered by ${note}`,
-    openGraph: {
-      title: `Notes: ${note}`,
-      description: `Notes filter: ${note}`,
-      url: `https://08-zustand-phi-three.vercel.app//notes/filter/${note}`,
-      siteName: "NoteHub",
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: note,
-        },
-      ],
-      type: "article",
-    },
-  };
-}
 
 export default async function Page({ params }: Props) {
   const { slug = [] } = await params;
